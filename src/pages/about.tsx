3cadbf5837b88a918bfed87e977a6daf13c65ca6
Elliott Hindman <shinjukudev@gmail.com>
2019-12-15 19:44:06 +0900

    Fix attribute order

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -54,13 +54,13 @@ const About: React.SFC = () => (
             <div className="post-content">
               <div className="responsive-iframe">
                 <iframe
-                  className="loading-element shimmer-animation"
                   src="https://player.vimeo.com/video/303992016"
                   width="640"
                   height="360"
                   frameBorder="0"
                   allow="autoplay; fullscreen"
                   allowFullScreen
+                  className="loading-element shimmer-animation"
                 ></iframe>
               </div>
               <p>
